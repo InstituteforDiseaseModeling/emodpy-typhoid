e66a13a84e0061376bdd72860adec946ef998f08
Jonathan Bloedow <jhb4@cornell.edu>
2023-09-28 07:46:48 -0700

    Added doc strings from ChatGPT for TV.

--- emodpy_typhoid/interventions/typhoid_vaccine.py
+++ emodpy_typhoid/interventions/typhoid_vaccine.py
@@ -5,8 +5,21 @@ import json
 
 def new_intervention( camp, efficacy=0.82, mode="Shedding", constant_period=0, decay_constant=6935.0 ):
     """
-    TyphoidVaccine intervention wrapper. Just the intervention. No configuration yet.
-    """
+     Create a new TyphoidVaccine intervention with specified parameters. If you use this function directly,
+	you'll need to distribute the intervention with a function like ScheduledCampaignEvent or TriggeredCampaignEvent
+	from emod_api.interventions.common.
+
+     Args:
+         camp (Camp): The camp to which the intervention is applied.
+         efficacy (float, optional): The efficacy of the Typhoid vaccine. Default is 0.82.
+         mode (str, optional): The mode of the intervention. Default is "Shedding".
+         constant_period (float, optional): The constant period of the waning effect in days. Default is 0.
+         decay_constant (float, optional): The decay time constant for the waning effect. Default is 6935.0.
+
+     Returns:
+         TyphoidVaccine: A fully configured instance of the TyphoidVaccine intervention with the specified parameters.
+     """
+
     intervention = s2c.get_class_with_defaults( "TyphoidVaccine", camp.schema_path )
     intervention.Effect = efficacy
     intervention.Mode = mode
@@ -30,8 +43,22 @@ def new_triggered_intervention(
         co_event=None # expansion slot
     ):
     """
-    Distribute TyphoidVaccine when something happens as determined by a signal published from the
-    model or another campaign event.
+    Create a new triggered TyphoidVaccine intervention based on specified parameters.
+    Args:
+         camp (Camp): The camp to which the intervention is applied.
+         efficacy (float, optional): The efficacy of the Typhoid vaccine. Default is 0.82.
+         mode (str, optional): The mode of the intervention. Default is "Shedding".
+         constant_period (float, optional): The constant period of the waning effect in days. Default is 0.
+         decay_constant (float, optional): The decay time constant for the waning effect. Default is 6935.0.
+         start_day (int, optional): The day on which the intervention starts. Default is 1.
+         triggers (list, optional): List of triggers for the intervention. Default is ["Births"].
+         coverage (float, optional): Demographic coverage of the intervention. Default is 1.0.
+         node_ids (list, optional): List of node IDs where the intervention is applied. Default is None.
+         property_restrictions_list (list, optional): List of property restrictions for the intervention. Default is an empty list.
+         co_event (None, optional): Expansion slot for future use.
+
+     Returns:
+         TriggeredCampaignEvent: An instance of a triggered campaign event with the TyphoidVaccine intervention.
     """
     iv = new_intervention( camp, efficacy=efficacy, mode=mode, constant_period=constant_period, decay_constant=decay_constant )
 
@@ -52,8 +79,22 @@ def new_scheduled_intervention(
         co_event=None # expansion slot
     ):
     """
-    Distribute TyphoidVaccine when something happens as determined by a signal published from the
-    model or another campaign event.
+    Create a new scheduled TyphoidVaccine intervention based on specified parameters.
+    Args:
+         camp (Camp): The camp to which the intervention is applied.
+         efficacy (float, optional): The efficacy of the Typhoid vaccine. Default is 0.82.
+         mode (str, optional): The mode of the intervention. Default is "Shedding".
+         constant_period (float, optional): The constant period of the waning effect in days. Default is 0.
+         decay_constant (float, optional): The decay time constant for the waning effect. Default is 6935.0.
+         start_day (int, optional): The day on which the intervention starts. Default is 1.
+         coverage (float, optional): Demographic coverage of the intervention. Default is 1.0.
+         node_ids (list, optional): List of node IDs where the intervention is applied. Default is None.
+         property_restrictions_list (list, optional): List of property restrictions for the intervention. Default is an empty list.
+         co_event (None, optional): Expansion slot for future use.
+
+     Returns:
+         ScheduledCampaignEvent: An instance of a scheduled campaign event with the TyphoidVaccine intervention.
+    
     """
     iv = new_intervention( camp, efficacy=efficacy, mode=mode, constant_period=constant_period, decay_constant=decay_constant )
 
